3e55230b2c8f43889e34bac3d5b3a88c4689bc1c
gamersinghji056-lang <gamersinghji056@gmail.com>
2026-09-06 20:23:38 +0530

    fix(whatsapp): remove invalid BOM from V6 scene

--- services/whatsapp/src/public-site/components/landing/LandingPage.tsx
+++ services/whatsapp/src/public-site/components/landing/LandingPage.tsx
@@ -1,4 +1,4 @@
-﻿import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { LandingAbout } from "./LandingAbout";
 import { LandingDownloadCta } from "./LandingDownloadCta";
 import { LandingFilesMedia } from "./LandingFilesMedia";
--- services/whatsapp/src/public-site/components/landing/scene/ProductScene.tsx
+++ services/whatsapp/src/public-site/components/landing/scene/ProductScene.tsx
@@ -1,4 +1,4 @@
-﻿ï»¿import { Laptop3D } from "./Laptop3D";
+ï»¿import { Laptop3D } from "./Laptop3D";
 
 export function ProductScene() {
   return (
